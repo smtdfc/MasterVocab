fd1db81ad760da94ba44b2a1dd398f25e626d7d6
smtdfc <smtdfc.master@gmail.com>
2025-07-15 15:07:57 +0700

    feat: Add systemInstruction for LLM

--- src/app/api/word/search-mean/route.ts
+++ src/app/api/word/search-mean/route.ts
@@ -5,6 +5,16 @@ const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY
 });
 
+const systemInstruction =  `
+You are an English-Vietnamese dictionary. When given an English word, respond with its most common Vietnamese meaning(s), in 2–3 words maximum.  
+Always respond in Vietnamese. Use commas to separate multiple meanings.  
+Do not explain, do not use full sentences, do not give examples, and do not provide any additional context.  
+Only output the direct translation(s).  
+Examples:
+- Hello -> Xin chào  
+- Bag -> Cái túi, cái cặp  
+- Run -> Chạy
+`,
 
 export async function POST(req: NextRequest) {
   const { word } = await req.json();
@@ -16,6 +26,7 @@ export async function POST(req: NextRequest) {
   
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
+    systemInstruction,
     contents: `What does the word '${word}' mean ?`,
   });
   
