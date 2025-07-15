50994918fda5be979d3d35213fc7a59af83cc9b7
smtdfc <smtdfc.master@gmail.com>
2025-07-15 15:52:41 +0700

    feat: Add page navigation

--- src/components/Offcanvas.tsx
+++ src/components/Offcanvas.tsx
@@ -2,12 +2,15 @@
 
 import { useUI } from '@/context/ui';
 import { useRef, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import '@/styles/offcanvas.css';
 
 
 export default function Offcanvas() {
   const { isOffcanvasOpen,closeOffcanvas } = useUI();
   const offcanvasRef = useRef < HTMLDivElement > (null);
+  const router = useRouter();
+  
   
   useEffect(() => {
     const el = offcanvasRef.current;
@@ -28,10 +31,10 @@ export default function Offcanvas() {
       </div>
       <div className="offcanvas-body">
         <ul className="offcanvas-menu">
-          <li>
+          <li onClick={() => router.push('/')}>
             <a>Home</a>
           </li>
-          <li>
+          <li onClick={() => router.push('/vocabulary')}>
             <a>Vocabulary</a>
           </li>
         </ul>
